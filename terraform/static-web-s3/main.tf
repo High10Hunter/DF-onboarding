8e28e6400293492388c7ca6883825b678cf877cf
High10Hunter <trandaiquy2903@gmail.com>
2024-05-13 19:14:21 +0700

    feat: upload file with terraform

--- terraform/static-web-s3/main.tf
+++ terraform/static-web-s3/main.tf
@@ -16,6 +16,21 @@ locals {
     Name        = "static-web-s3-h10h"
     Environment = "Development"
   }
+
+  mime_types = {
+    html  = "text/html"
+    css   = "text/css"
+    ttf   = "font/ttf"
+    woff  = "font/woff"
+    woff2 = "font/woff2"
+    js    = "application/javascript"
+    map   = "application/javascript"
+    json  = "application/json"
+    jpg   = "image/jpeg"
+    png   = "image/png"
+    svg   = "image/svg+xml"
+    eot   = "application/vnd.ms-fontobject"
+  }
 }
 
 resource "aws_s3_bucket" "static" {
@@ -30,13 +45,21 @@ resource "aws_s3_bucket_public_access_block" "static" {
 
   block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_acl" "static" {
   bucket = aws_s3_bucket.static.id
   acl    = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.static.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "static" {
@@ -51,19 +74,16 @@ resource "aws_s3_bucket_website_configuration" "static" {
   }
 }
 
-data "aws_iam_policy_document" "static" {
-  statement {
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.static.arn}/*"]
-
-    principals {
-      type = "*"
-      identifiers = ["*"]
-    }
-  }
-}
-
 resource "aws_s3_bucket_policy" "static" {
   bucket = aws_s3_bucket.static.id
-  policy = data.aws_iam_policy_document.static.json
+  policy = file("s3_static_policy.json")
+}
+
+resource "aws_s3_object" "object" {
+  for_each = fileset(path.module, "static-web/**/*")
+  bucket = aws_s3_bucket.static.id
+  key    = replace(each.value, "static-web", "")
+  source = each.value
+  etag         = filemd5("${each.value}")
+  content_type = lookup(local.mime_types, split(".", each.value)[length(split(".", each.value)) - 1])
 }
